20a7a91efbaeb6b93d03b5222a1d9aa397372792
Tanmay Singha <66204567+tanmaysingha@users.noreply.github.com>
2021-09-28 19:05:30 +0800

    Update SegNet.py

--- models/SegNet.py
+++ models/SegNet.py
@@ -1,4 +1,16 @@
 # -*- coding: utf-8 -*-
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """SegNet model for Keras.
 # Reference:
 - [Segnet: A deep convolutional encoder-decoder architecture for image segmentation](https://arxiv.org/pdf/1511.00561.pdf)
